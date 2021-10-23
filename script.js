de5e21dbcf464944239e3fb7785817aae4ab5152
OlgaP21 <73522702+OlgaP21@users.noreply.github.com>
2021-10-23 18:07:06 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,56 @@
 //task 4
+/*
 $.get("https://jsonkeeper.com/b/VST9", function(json_objects) {
+    for (object of json_objects) {
+        let section = $('<section>');
+        let div = $('<div>');
+        let p = $('<p class="grey">');
+        
+        let date = $('<p>').text(object.creationTime);
+
+        let faceImage = new Image();
+        faceImage.src = object.pathToFacePicture;
+        //faceImage.className = object.
+        p.append(faceImage);
+        p.append(date);
+
+        let pContent = $('<p>').text(object.bodyOfPost);
+
+        div.append(p);
+        if (object.pathToBodyPicture != null) {
+            let bodyImage = new Image();
+            bodyImage.src = object.pathToBodyPicture;
+            bodyImage.className = object.classOfPicture;
+            div.append(bodyImage);
+        }
+        div.append(pContent);
+
+        
+        let buttonClass = object.classOfButton;
+        $(div).append(
+            $(document.createElement('button')).prop({
+                type: 'button',
+                class: buttonClass
+            })
+        )
+
+        section.append(div);
+
+        $('body').append(section);
+    }
+})
+.fail (function(msg) {
+	alert("Please enable CORS (by installing corresponding extension)");
+})
+*/
+
+//task 5
+//run a local web server to retrieve data from a local file
+//to run web server open cmd in project directory
+//type a command: python -m http.server (for python version 3) or 
+//                python -m SimpleHTTPServer (for python version 2)
+//open index.html
+$.get("hw2.json", function(json_objects) {
     for (object of json_objects) {
         let section = $('<section>');
         let div = $('<div>');
